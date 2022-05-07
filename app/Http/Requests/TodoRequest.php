6acf29d0de1f388298f92df64547ac6351ef2c85
nesaitfirullail <nesaitfirullail12@gmail.com>
2022-05-07 13:44:31 +0700

    Praktikum 2 - membuat CRUD

---
+++ app/Http/Requests/TodoRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Models\User;
+use App\Http\Models\Todo;
+use Illuminate\Support\Facades\Hash;
+
+class TodoRequest extends ApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->method() = Request::METHOD_POST)
+            return true;
+        $todo = $this->route('todo');
+        return auth()->user()->id = $todo->user_id;    
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'todo' => 'required|string|max:255',
+            'label' => 'nullable|string',
+            'done' => 'nullable|boolean',
+        ];
+    }
+}
